bad20be263c9282d879b651046e03fafdd25f8a6
MakhamAroi <thatchaman19@gmail.com>
2025-09-02 12:08:15 +0700

    first commit

---
+++ abstractionCom.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+type (
+	Computer interface {
+		Input()
+	}
+
+	ComputerPC struct {
+		Brand        string
+		CPU          string
+		RAM          string
+		OS           string
+		power_supply string
+	}
+
+	ComputerLaptop struct {
+		Brand      string
+		CPU        string
+		RAM        string
+		OS         string
+		Battery    string
+		IsCanPress bool
+	}
+)
+
+func (m ComputerPC) Input() {
+	fmt.Println("Starting computer pressing turn on the computer PC: ", m.Brand, m.CPU, m.RAM, m.OS, m.power_supply)
+}
+
+func (m ComputerLaptop) Input() {
+	fmt.Println("Starting computer pressing turn on the computer laptop: ", m.Brand, m.CPU, m.RAM, m.OS, m.Battery, m.IsCanPress)
+}
+
+func main() {
+	computerPC := ComputerPC{
+		Brand:        "Dell",
+		CPU:          "Intel Core i7",
+		RAM:          "16GB",
+		OS:           "Windows,MacOS",
+		power_supply: "65W",
+	}
+
+	computerLaptop := ComputerLaptop{
+		Brand:      "HP",
+		CPU:        "AMD Ryzen 5",
+		RAM:        "8GB",
+		OS:         "Windows, Linux",
+		Battery:    "56Wh",
+		IsCanPress: true,
+	}
+
+	computerPC.Input()
+	computerLaptop.Input()
+}
